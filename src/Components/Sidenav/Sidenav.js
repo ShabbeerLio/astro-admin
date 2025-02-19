c4e9e893a52acc745eccb146d7d19c59f966d2df
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2025-02-19 18:27:05 +0530

    icon changed

--- src/Components/Sidenav/Sidenav.js
+++ src/Components/Sidenav/Sidenav.js
@@ -6,14 +6,15 @@ import logo from "../../Assets/astrology.png"
 const Sidenav = () => {
     let history = useNavigate();
     const handleLogout = () => {
-      localStorage.removeItem("token");
-      history("/login");
+        localStorage.removeItem("token");
+        history("/login");
     };
     return (
         <div className='Sidenav'>
             <div className="Sidenav-icon">
                 <Link to={"/"}>
                     <img src={logo} alt="" />
+                    <p>Astro Nivish</p>
                 </Link>
             </div>
             <div className='Sidenav-box'>
@@ -21,10 +22,10 @@ const Sidenav = () => {
                     <li>
                         <Link to={"/"}>Home</Link>
                     </li>
-                    {/* <li>
+                    <li>
                         <Link to={"/"}>Gochar</Link>
                     </li>
-                    <li>
+                    {/* <li>
                         <Link to={"/"}>User</Link>
                     </li> */}
                 </ul>
